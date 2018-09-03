f14fbe99d179ef33a0030fe249cdd6a5758a0bab
Jonathon Blok <jonathon@jblok.co.uk>
2018-09-03 09:00:26 +0100

    change sidebar copy, and move to netlify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,14 @@
-module.exports = function(grunt){
-
+module.exports = function(grunt) {
   // define the url the site is being deployed to - used for defining resource paths
   var env = grunt.option('env');
-  if (env == "prod"){
-    var base_url = "http://www.jblok.co.uk";
-  }
-  else {
-    var base_url = "http://localhost:5609/";
+  if (env == 'prod') {
+    var base_url = '/';
+  } else {
+    var base_url = 'http://localhost:5609/';
   }
 
   // get the work data from external json file
-  var work = grunt.file.readJSON("work.json");
-  var secret = grunt.file.readJSON("secret.json");
-
+  var work = grunt.file.readJSON('work.json');
 
   // set up preset grunt config - bake on index.html, build folder clean and assets copying
   var grunt_config = {
@@ -26,58 +22,48 @@ module.exports = function(grunt){
           }
         },
         files: {
-          "build/index.html": "src/index.html"
+          'build/index.html': 'src/index.html'
         }
       }
     },
-    clean: [ "build" ],
+    clean: ['build'],
     copy: {
       default: {
         files: [
           {
             expand: true,
-            cwd: "src",
-            src: ['assets/css/**', 'assets/js/**','images/fulls/**', 'images/thumbs/**', 'LICENSE.txt', 'README.txt'],
+            cwd: 'src',
+            src: [
+              'assets/css/**',
+              'assets/js/**',
+              'images/fulls/**',
+              'images/thumbs/**',
+              'LICENSE.txt',
+              'README.txt'
+            ],
             dest: 'build/'
-          },
+          }
         ]
       }
     },
     sass: {
       default: {
         options: {
-          style: "compressed"
+          style: 'compressed'
         },
         files: {
           'build/assets/css/main.css': 'src/assets/sass/main.scss',
           'build/assets/css/ie8.css': 'src/assets/sass/ie8.scss'
         }
       }
-    },
-    environments: {
-      prod: {
-        options: {
-          local_path: 'build',
-          current_symlink: 'public_html',
-          deploy_path: '/home/admin/web/jblok.co.uk',
-          host: secret.prod.host,
-          port: secret.prod.port,
-          username: secret.prod.username,
-          password: secret.prod.password,
-          debug: true,
-          releases_to_keep: '3'
-        }
-      }
     }
-  }
-
+  };
 
   // define the tasks to run
-  var tasks = [ "clean", "copy", "bake:index", "sass" ];
-
+  var tasks = ['clean', 'copy', 'bake:index', 'sass'];
 
   //add bake tasks for each work item to generate a new page
-  work.forEach(function(item, i){
+  work.forEach(function(item, i) {
     var task = {
       options: {
         content: {
@@ -88,13 +74,12 @@ module.exports = function(grunt){
       files: {}
     };
 
-    task.files["build/work/" + item.name + ".html"] = "src/templates/work.html";
+    task.files['build/work/' + item.name + '.html'] = 'src/templates/work.html';
 
     grunt_config.bake[item.name] = task;
 
-    tasks.push("bake:"+item.name);
+    tasks.push('bake:' + item.name);
   });
-
 
   // todos:
   // mobile external link button is a bit wierd
@@ -112,5 +97,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-ssh-deploy');
   grunt.loadNpmTasks('grunt-bake');
 
-  grunt.registerTask( "default", tasks);
+  grunt.registerTask('default', tasks);
 };
